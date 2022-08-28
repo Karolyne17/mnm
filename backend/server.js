16e65e57b5c230d6ef54a17cd5d158d0d2b0fa19
GH-ifa <guillaume.hoffmeyer@stagiairesifa.fr>
2022-08-28 15:05:12 +0200

    ✨ Modèles de données sequelize

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-// app.use(cors({ origin: "http://localhost:8080" }));
+app.use(cors({ origin: "http://localhost:4200" }));
 
 const db = require("./models");
 
